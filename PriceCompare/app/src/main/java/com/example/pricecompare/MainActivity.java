36950de7c57827914b86a26b96e25313112bb91f
BirajSinghGCUTA <birajsinghgc@gmail.com>
2021-04-10 12:58:58 -0500

    Initial File

---
+++ PriceCompare/app/src/main/java/com/example/pricecompare/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.pricecompare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //Create a new activity. Avoid changing this file and activity_main_xml.
+    //Android app opens in this file so If you wish to run your work:
+    //1. add a button widget on the activity main xml file
+    //2. inflate your button widget ((button)R.id.button)
+    //3. and link it to the work that your doing using the onClickListener.
+    //4. This files will remain unchanged in the main branch. By the end of this sprint, this file will be convertetd viewlist screen.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
