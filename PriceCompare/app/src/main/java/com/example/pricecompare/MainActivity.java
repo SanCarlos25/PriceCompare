315fa83a8c6b122f72ece593979a6ad9bf3820a2
BirajSinghGCUTA <birajsinghgc@gmail.com>
2021-04-10 13:39:36 -0500

    Initial File

--- PriceCompare/app/src/main/java/com/example/pricecompare/MainActivity.java
+++ PriceCompare/app/src/main/java/com/example/pricecompare/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     //1. add a button widget on the activity main xml file
     //2. inflate your button widget ((button)R.id.button)
     //3. and link it to the work that your doing using the onClickListener.
+    //4. DO NOT COMMIT THE CHANGES YOU MAKE IN THIS FILE INTO THE MAIN BRANCH(you can do it in your branch) AS THIS MEANS ALL OF US WILL BE WORKING
+    //          IN THE SAME FILE, Which is likely to create a merge error
     //4. This files will remain unchanged in the main branch. By the end of this sprint, this file will be convertetd viewlist screen.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
